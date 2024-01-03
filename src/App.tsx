7fb122e7a89665d680a1f6da34d433484649cf4a
Huski-commando <gyan.mu@gmail.com>
2024-01-03 12:51:47 +0530

    going to implement bitbucket

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { Button } from "./components/ui/button";
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
-      <Button variant="destructive" className="ml-4">
+      <Button variant="destructive" className="ml-4 uppercase">
         Testing Shad-cn UI
       </Button>
     </div>
